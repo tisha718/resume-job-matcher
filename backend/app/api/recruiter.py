6f1b064e33946f27c998f0a66868015cb33d600a
Roshan <roshan.bijoy-thomas@capgemini.com>
2026-01-02 17:35:28 +0530

    job creation working

---
+++ backend/app/api/recruiter.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.orm import Session
+
+from app.db.database import get_db
+from app.db.models import Job
+
+router = APIRouter(
+    prefix="/api/recruiter",
+    tags=["Job Management"]
+)
+
+@router.post("/jobs")
+async def create_job(
+    recruiter_id: int=Query(...),
+    title: str=Query(...),
+    description: str=Query(...),
+    location: str=Query(...),
+    job_type: str=Query(...),
+    db: Session = Depends(get_db)
+):
+    job=Job(
+        recruiter_id = recruiter_id,
+        title = title,
+        description = description,
+        location = location,
+        job_type = job_type
+    )
+
+    db.add(job)
+    db.commit()
+    db.refresh(job)
+
+    return {
+        "message": "Job created successfully",
+        "job_id": job.id,
+    }
