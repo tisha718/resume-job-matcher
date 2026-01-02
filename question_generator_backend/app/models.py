4883acd6daf0e993722f1067e58397aee54500a8
Kaviya <kaviyakarkuvel23@gmail.com>
2026-01-02 18:29:42 +0530

    deleted question_generator directory

--- question_generator_backend/app/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import List
-
-class QuestionResponse(BaseModel):
-    job_id: int = Field(..., example=1)
-    difficulty: str = Field(..., example="medium")
-    technical_questions: List[str]
-    behavioral_questions: List[str]
